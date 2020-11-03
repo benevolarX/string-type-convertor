d90aaad0a62bedfbd065d0422240067e14f321e6
benevolarX <benevolar.nouveau.project@gmail.com>
2020-11-03 20:24:46 +0100

    first commit for release

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # string-type-convertor
-transform string to typed value (number, boolean, float, null, undefined, ...)
+string-type-convertor is a js library for transform string to typed value (number, boolean, float, null, undefined, ...)
+It is useful when creating a router to retrieve the typed parameters of the url.
+
+## Installation
+
+Download [nodejs](https://nodejs.org/) first.
+Use [npm](https://www.npmjs.com/package/npm) to install string-type-convertor.
+
+```bash
+npm i string-type-convertor
+```
+
+## Usage
+
+```js
+import { typeConvertor } from 'string-type-convertor';
+
+typeConvertor("undefined"); // return undefined
+typeConvertor("null"); // return null
+typeConvertor("true"); // return true
+typeConvertor("false"); // return false
+typeConvertor("37n"); // return 37n
+typeConvertor("3.14"); // return 3.14
+typeConvertor("150"); // return 150
+typeConvertor("hello"); // return 'hello'
+
+```
+
+## Contributing
+I try to publish a small js helper.
+README generate by Google Translate (sorry for bad english)
+
+## License
+[MIT](https://github.com/benevolarX/string-type-convertor/blob/main/LICENSE)
---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/**!
+ * string-type-convertor
+ *
+ * @copyright 2020-2021 benevolarX
+ * @license MIT
+ */
+
+export const isBigInt = new RegExp(/^[0-9]+n$/);
+export const isFloat = new RegExp(/^[0-9]+\.[0-9]+$/);
+export const isInt = new RegExp(/^[0-9]+$/);
+
+/**
+ * 
+ * @param {string} val 
+ * @param {{ test: ((val: string) => boolean), convert: ((val: string) => any) }[]} tests 
+ */
+export function typeConvertor(val, tests = []) {
+
+    if (val === 'undefined') {
+        return undefined;
+    }
+
+    if (val === 'null') {
+        return null;
+    }
+
+    if (val === 'true') {
+        return true;
+    }
+
+    if (val === 'false') {
+        return false;
+    }
+    
+    if (isBigInt.test(val)) {
+        return BigInt(val.substr(0, val.length - 1));
+    }
+    
+    if (isFloat.test(val)) {
+        return parseFloat(val);
+    }
+
+    if (isInt.test(val)) {
+        return parseInt(val);
+    }
+
+    for (let i = 0; i < tests.length; i++) {
+        const { test, convert } = tests[i];
+        if (test(val)) {
+            return convert(val);
+        }
+    }
+
+    return val;
+
+}
